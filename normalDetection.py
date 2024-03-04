785d48487f5674e464827fdca8f9252aef929339
Thanh Nguyen <thanh.p.nguyen46@gmail.com>
2024-03-04 15:37:28 -0500

    Add DataFrame for training, testing, and validation

--- normalDetection.py
+++ normalDetection.py
@@ -26,6 +26,75 @@ warnings.filterwarnings("ignore")
 
 print ('modules loaded')
 
+# MAKING DATAFRAME FOR TRAINING, TESTING AND VALIDATION
+
+# TRAINING DATA
+# Generate data paths with labels
+train_data_dir = '/chest_xray/train'
+filepaths = []
+labels = []
+
+folds = os.listdir(train_data_dir)
+# print(folds)
+
+for fold in folds:
+    foldpath = os.path.join(train_data_dir, fold)
+    filelist = os.listdir(foldpath)
+    for file in filelist:
+        fpath = os.path.join(foldpath, file)
+        
+        filepaths.append(fpath)
+        labels.append(fold)
+
+# Concatenate data paths with labels into one dataframe
+Fseries = pd.Series(filepaths, name= 'filepaths')
+Lseries = pd.Series(labels, name='labels')
+
+train_df = pd.concat([Fseries, Lseries], axis= 1)
+train_df
+
+# TESTING DATA
+# Generate data paths with labels
+test_data_dir = '/chest_xray/test'
+filepaths = []
+labels = []
+
+folds = os.listdir(test_data_dir)
+for fold in folds:
+    foldpath = os.path.join(test_data_dir, fold)
+    filelist = os.listdir(foldpath)
+    for file in filelist:
+        fpath = os.path.join(foldpath, file)
+        
+        filepaths.append(fpath)
+        labels.append(fold)
+
+# Concatenate data paths with labels into one dataframe
+Fseries = pd.Series(filepaths, name= 'filepaths')
+Lseries = pd.Series(labels, name='labels')
+test_df = pd.concat([Fseries, Lseries], axis= 1)
+
+# VALIDATION DATA
+# Generate data paths with labels
+val_data_dir = '/chest_xray/val'
+filepaths = []
+labels = []
+
+folds = os.listdir(val_data_dir)
+for fold in folds:
+    foldpath = os.path.join(val_data_dir, fold)
+    filelist = os.listdir(foldpath)
+    for file in filelist:
+        fpath = os.path.join(foldpath, file)
+        
+        filepaths.append(fpath)
+        labels.append(fold)
+
+# Concatenate data paths with labels into one dataframe
+Fseries = pd.Series(filepaths, name= 'filepaths')
+Lseries = pd.Series(labels, name='labels')
+valid_df = pd.concat([Fseries, Lseries], axis= 1)
+
 # PREPROCESSING
 
 # crop image size
