871d4350f15cc062586276c07804991b8db4e29d
Thanh Nguyen <thanh.p.nguyen46@gmail.com>
2024-03-04 15:09:03 -0500

    Importing libraries

---
+++ normalDetection.py
@@ -0,0 +1,27 @@
+import os
+import itertools
+from PIL import Image
+
+# import data handling tools
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_style('darkgrid')
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# import Deep learning Libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout, BatchNormalization
+from tensorflow.keras import regularizers
+
+import warnings
+warnings.filterwarnings("ignore")
+
+print ('modules loaded')
