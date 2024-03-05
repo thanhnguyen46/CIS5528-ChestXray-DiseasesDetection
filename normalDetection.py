0593789eaf8a3cfe69eb862d2d25ceabd8aae7e5
Thanh Nguyen <thanh.p.nguyen46@gmail.com>
2024-03-04 23:02:03 -0500

    Add Model Structure Code

--- normalDetection.py
+++ normalDetection.py
@@ -139,3 +139,47 @@ for i in range(16):
     plt.axis('off')
 plt.tight_layout()
 plt.show()
+
+# MODEL STRUCTURE
+
+img_size = (224, 224)
+channels = 3
+img_shape = (img_size[0], img_size[1], channels)
+
+class_count = len(list(train_gen.class_indices.keys())) # to define number of classes in dense layer
+
+model = Sequential([
+    Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu", input_shape= img_shape),
+    Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu"),
+    MaxPooling2D((2, 2)),
+    
+    Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"),
+    MaxPooling2D((2, 2)),
+    
+    Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"),
+    MaxPooling2D((2, 2)),
+    
+    Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
+    MaxPooling2D((2, 2)),
+    
+    Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
+    Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
+    MaxPooling2D((2, 2)),
+    
+    Flatten(),
+    
+    Dense(256,activation = "relu"),
+    Dense(64,activation = "relu"),
+    Dense(class_count, activation = "softmax")
+])
+
+model.compile(Adamax(learning_rate= 0.001), loss= 'categorical_crossentropy', metrics= ['accuracy'])
+
+model.summary()
+
