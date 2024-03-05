868e1a232791243d71e6714bc7d686daa66a53ff
Thanh Nguyen <thanh.p.nguyen46@gmail.com>
2024-03-04 23:02:35 -0500

    Training the data

--- normalDetection.py
+++ normalDetection.py
@@ -183,3 +183,45 @@ model.compile(Adamax(learning_rate= 0.001), loss= 'categorical_crossentropy', me
 
 model.summary()
 
+# TRAINING THE DATA
+
+epochs = 13   # number of all epochs in training
+history = model.fit(train_gen, epochs= epochs, verbose= 1, validation_data= valid_gen, shuffle= False)
+
+tr_acc = history.history['accuracy']
+tr_loss = history.history['loss']
+val_acc = history.history['val_accuracy']
+val_loss = history.history['val_loss']
+index_loss = np.argmin(val_loss)
+val_lowest = val_loss[index_loss]
+index_acc = np.argmax(val_acc)
+acc_highest = val_acc[index_acc]
+
+Epochs = [i+1 for i in range(len(tr_acc))]
+loss_label = f'best epoch= {str(index_loss + 1)}'
+acc_label = f'best epoch= {str(index_acc + 1)}'
+
+# Plot training history
+plt.figure(figsize= (20, 8))
+plt.style.use('fivethirtyeight')
+
+plt.subplot(1, 2, 1)
+plt.plot(Epochs, tr_loss, 'r', label= 'Training loss')
+plt.plot(Epochs, val_loss, 'g', label= 'Validation loss')
+plt.scatter(index_loss + 1, val_lowest, s= 150, c= 'blue', label= loss_label)
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(Epochs, tr_acc, 'r', label= 'Training Accuracy')
+plt.plot(Epochs, val_acc, 'g', label= 'Validation Accuracy')
+plt.scatter(index_acc + 1 , acc_highest, s= 150, c= 'blue', label= acc_label)
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout
+plt.show()
